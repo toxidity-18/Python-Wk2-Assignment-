c33f4477b025be1961cceb9fce9166caf25b2023
toxidity-18 <desavigatunganga@gmail.com>
2025-08-12 09:04:23 +0300

    extending elements , removing the last element,sort in ascending order,print out an index

--- Built_In_Data_Structure.py
+++ Built_In_Data_Structure.py
@@ -9,3 +9,17 @@ print(my_list)
 # insert value 15 at the second position in the list .
 my_list.insert(1,15)
 print(my_list)
+# extending extra elements 
+another_list=[50,60,70]
+my_list.extend(another_list)
+print(my_list)
+# Remove the last element inside my list 
+del my_list[-1]
+print(my_list)
+# sort my_list in ascending order .
+my_list.sort()
+print(my_list)
+# print out the index of value 30 
+index_of_30 = my_list.index(30)
+print(index_of_30)
+# print(my_list[3]) : confirmation .
